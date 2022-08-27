10af5ee05e0fda0379d57ffb236bd1a342379bc1
Hector Vazquez <hdvs96@hotmail.com>
2022-08-27 00:24:31 -0500

    probando videos aber

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,9 +5,13 @@ import styles from '../styles/Home.module.css'
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      <Typography variant="h1" component="h2">
-        Hola mundo
+      <Typography variant="h1" component="h2" color={'purple'}>
+        Hola amorsito te amo mucho 
       </Typography>
+      <video width="480" height="480" controls>
+        <source src="../resource/videoChido.mp4" type="video/mp4"/>
+      Your browser does not support the video tag.
+      </video>
     </div>
   )
 }
